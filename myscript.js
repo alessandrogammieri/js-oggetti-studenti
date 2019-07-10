2666ac6e2bf49d3df84b14dfcaa1e844a7d42e7f
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-10 18:56:57 +0200

    array student objects + (firstname + lastname)

--- myscript.js
+++ myscript.js
@@ -6,7 +6,7 @@ var student, print, display = " ";
 
 // Oggetto studente
 student = {
-  fistName: "Mario",
+  firstName: "Mario",
   lastName: "Rossi",
   age: 45
 };
@@ -24,10 +24,39 @@ document.getElementById('mio_id').innerHTML = display;
 /* Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e
 stampare per ognuno nome e cognome */
 
+// Dichiarazione variabili
+var school, key, stamp = " ";
 
+// Oggetti di studenti
+student1 = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 45
+};
 
+student2 = {
+  firstName: "Luca",
+  lastName: "Bianchi",
+  age: 35
+};
 
+student3 = {
+  firstName: "Maria",
+  lastName: "Verdi",
+  age: 28
+};
 
+// Array di oggetti di studenti
+school = [student1, student2, student3];
+
+// Ciclo for
+for (var i = 0; i < school.length; i++) {
+  console.log(school[i].firstName, school[i].lastName);
+  stamp = stamp + school[i].firstName + school[i].lastName + " ";
+}
+
+// Stampiamo a schermo
+document.getElementById('tuo_id').innerHTML = stamp;
 
 
 /* Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo
